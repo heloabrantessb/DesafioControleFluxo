4afb7ed2bb6f00425085c8cbabc875d136d11d8d
heloabrantessb <heloisa.abrantesdev@gmail.com>
2024-09-25 12:31:44 -0300

    Finalização do código de contagem

--- src/Contador.java
+++ src/Contador.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class Contador {
     public static void main(String[] args) {
-        
+        Scanner teclado = new Scanner(System.in);
+
+        int parametroUm, parametroDois;
+
+        System.out.println("Digite o primeiro número");
+        parametroUm = teclado.nextInt();
+        System.out.println("Digite o segundo número");
+        parametroDois = teclado.nextInt();
+
+        try {
+            contagem(parametroUm, parametroDois);
+        } catch (ParametrosInvalidosException e) {
+            System.out.println("<ERRO>: O SEGUNDO NÚMERO DEVE SER MAIOR QUE O PRIMEIRO.");
+        }
+    }
+
+    public static void contagem(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        if (parametroDois < parametroUm)
+            throw new ParametrosInvalidosException();
+
+        int contagem = 0;
+        for (int i = parametroUm + 1; i <= parametroDois; i++) {
+            System.out.println("Imprimindo o número " + i);
+            contagem++;
+        }
+        System.out.println("O número de contagens foi: " + contagem);
     }
 }
